3b586ec205f4e4b943eb5b740c9eb6f0b60674e3
huanhuanjs <18500254217@163.com>
2016-03-28 11:10:12 +0800

    Update server.js
    version 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var os = require("os");
 http.createServer(function(request, response) {
     console.log("New request arrived.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(""Welcome to my website!It's the version 2 \n");
+    response.write("Welcome to my website!It is the version 2 \n");
     response.end();
 }).listen(80);
 console.log("Server is running, listening on port 80....");
